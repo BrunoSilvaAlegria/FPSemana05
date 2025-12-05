35b0010b72b3db1340e6b3ae9d009838a704f0ad
Bruno Alegria <bruno.alegria.lusofona@gmail.com>
2025-12-05 16:08:22 +0000

    Finish banco.py functions

--- banco.py
+++ banco.py
@@ -1,9 +1,29 @@
 import argparse
 
-
 class ContaBancaria:
-    pass
+    def __init__(self, titular, saldo, limite):
+        self.titular = titular
+        self.saldo = saldo
+        self.limite = limite
 
+    def depositar(self, valor):
+        self.saldo += valor
+        if valor > 0: print(1)
+        else: print(0)
+
+    def levantar(self, valor):
+        if valor <= self.saldo:
+            self.saldo -= valor
+            print(1)
+        else: print(0)
+    
+    def exibir_saldo(self):
+        print(f"{self.saldo:.2f}")
+
+    def exibir_info(self):
+        print(f"[{self.titular}] [{self.saldo:.2f}] [{self.limite}]")
+               
+        
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Gerir Uma Conta Bancária")
